13dc1be09a0afb9a0223572df31846a728172005
jdcampolargo <jdcampolargo@hotmail.com>
2023-01-10 16:34:38 -0600

    initial commit

---
+++ content.js
@@ -0,0 +1,29 @@
+const activationKey = "x";
+let activationKeyDown = false;
+document.addEventListener('keydown', (event) => {
+  if (event.key.toLowerCase() === activationKey) {
+    activationKeyDown = true;
+  }
+});
+document.addEventListener('keyup', (event) => {
+  if (event.key.toLowerCase() === activationKey) {
+    activationKeyDown = false;
+  }
+});
+
+document.addEventListener('copy', (event) => {
+  if (activationKeyDown) {
+    const selection = document.getSelection();
+    event.clipboardData.setData('text/plain', selection.toString() + "\n\nSource: " + window.location.href);
+    event.preventDefault();
+    // whatever i highlight it's automatically copied
+    // to clipboard and i can paste it anywhere
+
+
+
+
+
+  }
+});
+
+
